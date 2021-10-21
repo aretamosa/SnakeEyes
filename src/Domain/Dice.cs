cf533054ba3cff6a3e5ce63d013e885798717c72
Alicia Retamosa <a.retamosa@alumnos.upm.es>
2021-10-21 09:55:12 +0200

    Implement Domain

---
+++ src/Domain/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Dice
+    {
+        private Random _randomizer = new();
+        public int Dots { get; private set; }
+
+        public Dice() {
+            Dots = 6;
+        }
+
+
+        private readonly Random _random = new Random();
+        public int Roll()
+        {
+            return _randomizer.Next(1, 7);
+        }
+    }
+}
---
+++ src/Domain/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Game
+    {
+        private Dice _dice1;
+        private Dice _dice2;
+        private readonly List<int> _highscores = new();
+
+        public int Eye1 => _dice1.Dots;
+        public int Eye2 => _dice2.Dots;
+        public bool HasSnakeEyes => Eye1 == 1 && Eye2 == 1;
+        public IReadOnlyList<int> HighScores => _highscores.AsReadOnly();
+        public int Total { get; private set; }
+
+        public Game()
+        {
+            Initilize();
+        }
+
+        private void Initilize()
+        {
+            _dice1 = new Dice();
+            _dice2 = new Dice();
+        }
+
+        private void Play()
+        {
+            _dice1.Roll();
+            _dice2.Roll();
+
+            if (HasSnakeEyes)
+            {
+                _highscores.Add(Total);
+                Total = 0;
+            }
+            else
+            {
+                Total += Eye1 + Eye2;
+            }
+        }
+
+        private void Restart()
+        {
+            Initilize();
+        }
+    }
+}
